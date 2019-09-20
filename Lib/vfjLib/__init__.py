1b99ee4c50ff0d0ed2d335e7aa96f4abdb3b2231
Vassil Kateliev <vassil@kateliev.com>
2019-09-20 13:49:45 +0300

    Update __init__.py

--- Lib/vfjLib/__init__.py
+++ Lib/vfjLib/__init__.py
@@ -20,7 +20,7 @@ from collections import defaultdict
 
 
 # - Init ---------------------------------------------
-__version__ = '0.1.5'
+__version__ = '0.1.7'
 
 # - Classes ------------------------------------------
 class attribdict(defaultdict):
